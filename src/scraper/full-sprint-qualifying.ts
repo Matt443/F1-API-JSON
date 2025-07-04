f95844131f96f8143bcb6961e226a76ec6f322ca
Matt443 <marcin26050606@gmail.com>
2025-07-04 19:32:27 +0200

    Added getFullSprintShootout to readme.md fixed type

--- src/scraper/full-sprint-qualifying.ts
+++ src/scraper/full-sprint-qualifying.ts
@@ -1,21 +1,21 @@
 import { staticLinks } from "../endpoints/endpoints";
 
-import { isFullRaceResult } from "../types/types";
+import { fullQualiResult } from "../types/types";
 import { assignQualiValues, getF1Table, getResultURL } from "../utils/scrapping";
 
 /**
  *
  * @param {year} year - 1950-now
  * @param {string} [raceName="Australia"]
- * @returns {Promise<isFullRaceResult[]>}
+ * @returns {Promise<fullQualiResult[]>}
  */
 
-export const getFullSprintShootout = async (year: number = new Date().getFullYear(), raceName: string = "Brazil"): Promise<isFullRaceResult[]> => {
+export const getFullSprintShootout = async (year: number = new Date().getFullYear(), raceName: string = "Brazil"): Promise<fullQualiResult[]> => {
     try {
         const resultsURL = await getResultURL(year, raceName);
         const sprintQualiURL = `${staticLinks.fullResults}/${year}/${resultsURL.slice(23, resultsURL.length - 11)}/sprint-qualifying`;
 
-        return getF1Table(sprintQualiURL, assignQualiValues) as unknown as isFullRaceResult[];
+        return getF1Table(sprintQualiURL, assignQualiValues) as unknown as fullQualiResult[];
     } catch (error: unknown) {
         throw new Error(error as string);
     }
